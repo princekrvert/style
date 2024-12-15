069681325ec57cf0a629088fc72bdd6a74954560
prince <prince@localhost.localdomain>
2024-12-15 16:01:25 +0530

    This is style basic update

--- install.sh
+++ install.sh
@@ -2,4 +2,23 @@
 # Author Prince kumar 
 # this scritp is going to be insallation of all the addtional script in bash...
 # date 28 nov 2024
-echo -ne "This is prince kumar\n"
+# first check for the os 
+command -v go 2>&1 > /dev/null || { echo -e  "${g}+++++${y}Installing golang ${g}+++++" ; apt-get install golang -y ; }
+check_os=$(uname -o)
+if [[ $check_os == "GNU/Linux" ]];then 
+    # install the go 
+    # now make the binary and insatall the program to the path
+    go build -o style 
+    # Now move the binary to the bin/bash 
+    go install 
+elif [[ $check_os == "Android" ]]; then 
+    # install the go for android ..
+    go build -o style 
+    go install
+    mv style /data/data/com.termux/files/usr/bin
+else 
+    echo ""
+fi
+
+
+
